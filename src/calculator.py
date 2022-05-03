6b46c7128a33d2889a55f8988bca3571dae9db7b
jtjaws <89544761+jtjaws@users.noreply.github.com>
2022-05-03 12:22:38 -0500

    Create calculator.py

---
+++ src/calculator.py
@@ -0,0 +1,18 @@
+def plus(number_one, number_two):
+    return number_one + number_two
+
+
+def minus(number_one, number_two):
+    return number_one - number_two
+
+
+def divide(number_one, number_two):
+    return number_one / number_two
+
+
+def multiply(number_one, number_two):
+    return number_one * number_two
+
+
+def get_hello(who):
+    return "hello " + who
